fdc78dcc57978f13075c0e9da61203247e38f282
B2B\mansfieldr <rob.mansfield@proagrica.com>
2018-04-19 22:59:21 +0100

    Updating package init to reflect only outward facing functions

--- HPyCC/__init__.py
+++ HPyCC/__init__.py
@@ -1,5 +1,2 @@
-from hpycc.getHPCC import get_script_result
-
 from hpycc.get import get_output, get_outputs, save_output, save_outputs
-from hpycc.getfiles import getfiles
-from hpycc.getscripts import getscripts
+
