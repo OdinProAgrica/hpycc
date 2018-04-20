83f19c2d502ea268fa72e05ac5a6252be0191f7a
B2B\mansfieldr <rob.mansfield@proagrica.com>
2018-04-20 13:28:45 +0100

    Adding tests for syntax checker, also an init for the utils folder where it now lives

---
+++ tests/test_syntaxcheck.py
@@ -0,0 +1,17 @@
+import hpycc.utils.syntaxcheck as syntax
+import pytest
+
+
+def test_sntaxcheck_pass():
+    result = syntax.syntax_check('ECLtest_working.ecl', repo=None, silent=False)
+    assert result is None
+
+
+def test_sntaxcheck_warning():
+    with pytest.warns(UserWarning) as e_info:
+        syntax.syntax_check('ECLtest_warning.ecl', repo=None, silent=False)
+
+
+def test_sntaxcheck_fail():
+    with pytest.raises(Exception) as e_info:
+        syntax.syntax_check('ECLtest_fail.ecl', repo=None, silent=False)
