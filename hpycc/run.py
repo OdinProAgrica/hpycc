f8ce6fccc941ac87f9360547f07034d07b12925d
B2B\mansfieldr <rob.mansfield@proagrica.com>
2018-04-29 21:52:46 +0100

    Making a run and send class for calling externally. Run includes delete

---
+++ hpycc/run.py
@@ -0,0 +1,78 @@
+import logging
+import os
+from hpycc.get import LOG_PATH
+from hpycc.scriptrunning import runscript
+from hpycc.utils.logfunctions import boot_logger
+from hpycc.utils.HPCCconnector import HPCCconnector
+
+
+def run_script(script, server, port="8010", repo=None,
+               username="hpycc_get_output", password='" "',
+               legacy=False, do_syntaxcheck=True,
+               silent=False, debg=False, log_to_file=False, logpath=LOG_PATH):
+    """
+    Run an ECL script but do not download, save or process the response.
+
+
+    :param script: str
+         Path of script to execute.
+    :param server: str
+        IP address or url of the HPCC server, supply usernames, passwords and ports
+        using other arguments
+    :param port: str, optional
+        Port number ECL Watch is running on. "8010" by default.
+    :param repo: str, optional
+        Path to the root of local ECL repository if applicable.
+    :param username: str, optional
+        Username to execute the ECL workunit. "hpycc_get_output" by
+        default.
+    :param password: str, optional
+        Password to execute the ECL workunit. " " by default
+    :param legacy: bool, optional
+        Should ECL commands be sent with the -legacy flag, False by default
+    :param do_syntaxcheck: bool, optional
+        Should a syntaxcheck be completed on the script before running. True by default
+    :param silent: bool, optional
+        Should all feedback except warnings and errors be suppressed. False by default
+    :param debg: bool, optional
+        Should debug info be logged. False by default
+    :param log_to_file: bool, optional
+        Should log info be dumped to a file. False by default
+    :param logpath: str, optional
+        If logging to file, what is the filename? hpycc.log by default.
+
+    :return: None
+
+    """
+
+    boot_logger(silent, debg, log_to_file, logpath)
+    logger = logging.getLogger('run_script')
+    logger.debug('Starting run_script')
+
+    hpcc_server = HPCCconnector(server, port, None, username, password, None)
+    runscript.run_script_internal(script, hpcc_server, do_syntaxcheck)
+
+    return None
+
+
+def delete(logical_file, server, port="8010", repo=None,
+           username="hpycc_get_output", password='" "',
+           legacy=False, do_syntaxcheck=True,
+           silent=False, debg=False, log_to_file=False, logpath=LOG_PATH):
+
+    boot_logger(silent, debg, log_to_file, logpath)
+    logger = logging.getLogger('delete file')
+    logger.debug('Starting delete file')
+
+    script = "IMPORT std; STD.File.DeleteLogicalFile('%s');" % logical_file
+
+    script_loc = 'tempScript.ecl'
+    with open(script_loc, 'w') as f:
+        f.writelines(script)
+
+    hpcc_server = HPCCconnector(server, port, None, username, password, None)
+
+    runscript.run_script_internal(script_loc, hpcc_server, do_syntaxcheck)
+    os.remove(script_loc)
+
+    return None
