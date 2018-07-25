d1eaf6aecbf3de7ba6d56bfbca1d70f3c601ffc1
Jack Cooper <jack.cooper@reedbusiness.com>
2018-07-25 11:11:10 +0100

    tests for parse_schema_xml

--- tests/test_without_server/utils/test_parsers.py
+++ tests/test_without_server/utils/test_parsers.py
@@ -1,6 +1,10 @@
 import unittest
 
-from hpycc.utils.parsers import parse_wuid_from_failed_response, parse_wuid_from_xml
+from hpycc.utils.parsers import (
+    parse_wuid_from_failed_response,
+    parse_wuid_from_xml,
+    parse_schema_from_xml
+)
 
 
 class TestParseWUIDFromFailedResponseWithoutServer(unittest.TestCase):
@@ -61,3 +65,119 @@ class TestParseWUIDFromXMLWithoutServer(unittest.TestCase):
         string = self.xml.format("W1234567")
         res = parse_wuid_from_xml(string)
         self.assertIsNone(res)
+
+
+class TestParseSchemaFromXML(unittest.TestCase):
+    def setUp(self):
+        self.bare_schema = "\n".join([
+                ('<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" '
+                 'xmlns:hpcc="urn:hpccsystems:xsd:appinfo" elementFormDefault='
+                 '"qualified" attributeFormDefault="unqualified">'),
+                ('<xs:element name="Dataset"><xs:complexType><xs:sequence '
+                 'minOccurs="0" maxOccurs="unbounded">'),
+                '<xs:element name="Row"><xs:complexType><xs:sequence>',
+                '{}',
+                ('<xs:element name="__fileposition__" type='
+                 '"xs:nonNegativeInteger"/>'),
+                '</xs:sequence></xs:complexType></xs:element>',
+                '</xs:sequence></xs:complexType></xs:element>',
+                '</xs:schema>'
+            ])
+        self.column_schema = '<xs:element name="int" type="{}"/>'
+        self.set_schema = "\n".join([
+            '<xs:element name="set_int">',
+            ('<xs:complexType><xs:sequence><xs:element name="All" '
+             'minOccurs="0"/>'),
+            ('<xs:element name="Item" minOccurs="0" maxOccurs="unbounded" '
+             'type="{}"/>'),
+            '</xs:sequence></xs:complexType></xs:element>'
+        ])
+
+    def test_parse_schema_from_xml_parses_types_correctly(self):
+        types = [
+            ("xs:integer", int),
+            ("xs:nonNegativeInteger", int),
+            ("decimal10", float),
+            ("decimal5", float),
+            ("decimal5_3", float),
+            ("decimal8_2", float),
+            ("udecimal10", float),
+            ("udecimal5", float),
+            ("udecimal5_3", float),
+            ("udecimal8_2", float),
+            ("xs:double", float),
+            ("xs:string", str),
+            ("xs:boolean", bool),
+            ("xs:hexBinary", str),
+            ("data16", str),
+            ("data32", str),
+            ("string3", str),
+            ("string8", str)
+        ]
+        for t in types:
+            col = self.column_schema.format(t[0])
+            schema = self.bare_schema.format(col)
+            res = [i for i in parse_schema_from_xml(schema)][0]
+            self.assertEqual("int", res.name)
+            self.assertFalse(res.is_set)
+            self.assertEqual(res.type, t[1])
+
+    def test_parse_schema_from_xml_returns_file_position(self):
+        col = self.column_schema.format("data16")
+        schema = self.bare_schema.format(col)
+        res = [i for i in parse_schema_from_xml(schema)][1]
+        self.assertEqual(res.name, "__fileposition__")
+        self.assertFalse(res.is_set)
+        self.assertEqual(res.type, int)
+
+    def test_parse_schema_from_xml_returns_all_columns(self):
+        cols = [self.column_schema.format("string")] * 100
+        joined = "\n".join(cols)
+        schema = self.bare_schema.format(joined)
+        res = [i for i in parse_schema_from_xml(schema)][:-1]
+        self.assertEqual(100, len(res))
+
+    def test_parse_schema_from_xml_parses_set_types_correctly(self):
+        types = [
+            ("xs:integer", int),
+            ("xs:nonNegativeInteger", int),
+            ("decimal10", float),
+            ("decimal5", float),
+            ("decimal5_3", float),
+            ("decimal8_2", float),
+            ("udecimal10", float),
+            ("udecimal5", float),
+            ("udecimal5_3", float),
+            ("udecimal8_2", float),
+            ("xs:double", float),
+            ("xs:string", str),
+            ("xs:boolean", bool),
+            ("xs:hexBinary", str),
+            ("data16", str),
+            ("data32", str),
+            ("string3", str),
+            ("string8", str)
+        ]
+        for t in types:
+            col = self.set_schema.format(t[0])
+            schema = self.bare_schema.format(col)
+            res = [i for i in parse_schema_from_xml(schema)][0]
+            self.assertEqual("set_int", res.name)
+            self.assertTrue(res.is_set)
+            self.assertEqual(res.type, t[1])
+
+    def test_parse_schema_from_xml_parses_sets_and_cols(self):
+        col = self.column_schema.format("xs:string")
+        s = self.set_schema.format("xs:double")
+        sc = "\n".join([col, s])
+        schema = self.bare_schema.format(sc)
+        res = sorted(parse_schema_from_xml(schema), key=lambda x: x.name)
+        expected = [
+            ("__fileposition__", False, int),
+            ("int", False, str),
+            ("set_int", True, float)
+        ]
+        for e, r in zip(expected, res):
+            self.assertEqual(e[0], r[0])
+            self.assertEqual(e[1], r[1])
+            self.assertEqual(e[2], r[2])
